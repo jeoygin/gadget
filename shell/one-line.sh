52673bc19ab9fa662a4ac31f77d20d57abfd62ed
Jeoygin Wang <jeoygin@gmail.com>
2016-03-05 19:53:07 +0800

    Update one-line.sh

--- shell/one-line.sh
+++ shell/one-line.sh
@@ -3,3 +3,7 @@ date -j -f "%Y%m%d%H%M%S" "20160303010203" +"%s"
 
 # Calculate average value
 awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' FILE
+
+# Extract commits between revisions R1 and R2, and apply them on top of the current branch
+git format-patch -k --stdout R1..R2 | git am -3 -k
+
