030e5afa963b1f88b9aaec2dfb077fca8de479b9
Jeoygin Wang <jeoygin@gmail.com>
2016-03-21 19:47:33 +0800

    Update one-line-shell

--- shell/one-line.sh
+++ shell/one-line.sh
@@ -7,3 +7,6 @@ awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' FILE
 # Extract commits between revisions R1 and R2, and apply them on top of the current branch
 git format-patch -k --stdout R1..R2 | git am -3 -k
 
+# Download tarball and extract
+$ curl http://www.example.com/test.tar.gz | tar zx
+
