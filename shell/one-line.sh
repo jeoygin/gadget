3d5814dcc284854e7ddbd882e89ddf64d226e5f9
Jeoygin Wang <jeoygin@gmail.com>
2016-05-09 11:20:02 +0800

    Print dirname and basename in awk

--- shell/one-line.sh
+++ shell/one-line.sh
@@ -1,39 +1,59 @@
-# Change date format
-date -j -f "%Y%m%d%H%M%S" "20160303010203" +"%s"
+#
+# Common
+#
 
-# Calculate average value
-awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' FILE
-
-# Group by
-awk '{freq[$1]++} END {for (f in freq) print f, freq[f]}' FILE
-
-# Extract commits between revisions R1 and R2, and apply them on top of the current branch
-git format-patch -k --stdout R1..R2 | git am -3 -k
-
-# Download tarball and extract
-$ curl http://www.example.com/test.tar.gz | tar zx
-
-# rsync
-$ rsync -avP source destination
-
-# Download and write to file
-$ curl -sL -o /path/to/local/test.tar.gz http://www.example.com/test.tar.gz
-
-# Start HTTP Static Server
-$ python -m SimpleHTTPServer 8000 # python 2
-$ python -m http.server 8000 # python 3
-$ npm install -g http-server && http-server -p 8000 # Node.js
-$ curl https://gist.github.com/willurd/5720255 # for more other languages
-
-# Replace foo in last command with bar
+## Replace foo in last command with bar
 $ ^foo^bar
 
-# Create backup file
+## Create backup file
 $ cp filename{,.bak}
 
-# CentOS Package Manager
-$ yum list installed
-$ rpm -ql packageName
+#
+# Xargs
+#
+
+$ awk '{print $1,$2}' FILE | xargs -n 2 bash -c 'SHELL $0 + $1'
+
+#
+# Date
+#
+
+## Change date format
+date -j -f "%Y%m%d%H%M%S" "20160303010203" +"%s"
+
+#
+# Awk
+#
+
+## Calculate average value
+awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }' FILE
+
+## Group by
+awk '{freq[$1]++} END {for (f in freq) print f, freq[f]}' FILE
+
+## Pring path dirname and basename
+echo "/usr/local/bin"  | awk '{match($1, "[^/]+$"); print substr($1, 0, RSTART-1), substr($1, RSTART, RLENGTH)}'
+echo "/usr/local/bin"  | awk '{split($1, parts ,"/"); print parts[length(parts)]}'
+
+#
+# Git
+#
+
+## Extract commits between revisions R1 and R2, and apply them on top of the current branch
+git format-patch -k --stdout R1..R2 | git am -3 -k
+
+#
+# Data Download & Upload & Sync
+#
+
+## Download tarball and extract
+$ curl http://www.example.com/test.tar.gz | tar zx
+
+## Download and write to file
+$ curl -sL -o /path/to/local/test.tar.gz http://www.example.com/test.tar.gz
+
+## rsync
+$ rsync -avP source destination
 
 #
 # List Listening Network Port
@@ -48,5 +68,20 @@ $ lsof -i :$port -nP
 ## Linux
 $ netstat -ntlp
 
-# xargs
-$ awk '{print $1,$2}' FILE | xargs -n 2 bash -c 'SHELL $0 + $1'
+#
+# HTTP Static Server
+#
+
+$ python -m SimpleHTTPServer 8000 # python 2
+$ python -m http.server 8000 # python 3
+$ npm install -g http-server && http-server -p 8000 # Node.js
+$ curl https://gist.github.com/willurd/5720255 # for more other languages
+
+#
+# Linux Distribution Package Manager
+#
+
+## CentOS Package Manager
+$ yum list installed
+$ rpm -ql packageName
+
