1dcdc1e1342795a4b1829a03a991997c3c68327a
Jeoygin Wang <jeoygin@gmail.com>
2016-07-06 16:50:48 +0800

    Improve image binary

--- images/binary/binary.cpp
+++ images/binary/binary.cpp
@@ -4,63 +4,114 @@
 #include <sys/stat.h>
 #include <libgen.h>
 #include <iostream>
-#include "binary-img.h"
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 using namespace cv;
 using namespace std;
 
-int makedirs(char * path, mode_t mode) {
-    struct stat st = {0};
+namespace po = boost::program_options;
 
-    if (stat(path, &st) == 0) {
-        if (S_ISDIR(st.st_mode) == 0) {
-            return -1;
-        }
-        return 0;
-    }
-
-    char subpath[512] = "";
-    char * delim = strrchr(path, '/');
-    if (delim != NULL) {
-        strncat(subpath, path, delim - path);
-        makedirs(subpath, mode);
-    }
-    if (mkdir(path, mode) != 0) {
-        return -1;
-    }
-    return 0;
+void print_usage(string app_name, po::options_description desc) {
+  cout << "Usage: " << app_name << " [options]" << endl << endl;
+  cout << desc << endl;
 }
 
-Mat doit(Mat src) {
-  Mat gray, dst;
-  cvtColor( src, gray, CV_BGR2GRAY );
-  Mat invert = (255 - gray);
-  binary_v5::Img_Binary( invert, dst );
-  // threshold( src, dst, 80, 255, 0 );
-  return (255 - dst);
+int makedirs(char * path, mode_t mode) {
+  struct stat st = {0};
+
+  if (stat(path, &st) == 0) {
+    if (S_ISDIR(st.st_mode) == 0) {
+      return -1;
+    }
+    return 0;
+  }
+
+  char subpath[512] = "";
+  char * delim = strrchr(path, '/');
+  if (delim != NULL) {
+    strncat(subpath, path, delim - path);
+    makedirs(subpath, mode);
+  }
+  if (mkdir(path, mode) != 0) {
+    return -1;
+  }
+  return 0;
+}
+
+void binary_image(const string& srcpath, const string& dstpath) {
+  char buf[512];
+  struct stat st = {0};
+  strncpy(buf, dstpath.c_str(), sizeof(buf));
+  char * dir = dirname(buf);
+  makedirs(dir, 0775);
+
+  Mat gray = imread(srcpath, CV_LOAD_IMAGE_GRAYSCALE);
+  Mat blur, bin;
+
+  threshold(gray, bin, 0, 255, CV_THRESH_BINARY|CV_THRESH_OTSU);
+
+  if (!dstpath.empty()) {
+    imwrite(dstpath, bin);
+  } else {
+    imshow("binary", bin);
+    waitKey();
+  }
 }
 
 int main(int argc, char** argv) {
-  char buf[512];
-  if (argc >= 3) {
-    struct stat st = {0};
-    strncpy(buf, argv[2], sizeof(buf));
-    char * dir = dirname(buf);
-    makedirs(dir, 0775);
-    Mat src = imread(argv[1], 0);
-    Mat dst = doit(src);
-    imwrite(argv[2], dst);
+  string app_name = boost::filesystem::basename(argv[0]);
+  bool stdin = false;
+  string srcpath, dstpath;
+
+  po::options_description desc("Options");
+  desc.add_options()
+    ("help", "print help messages")
+    ("stdin", "read parameters from standard input")
+    ("input,i", po::value<string>(&srcpath), "input image")
+    ("output,o", po::value<string>(&dstpath), "output image");
+
+  po::positional_options_description positionalOptions;
+  positionalOptions.add("input", 1);
+  positionalOptions.add("output", 1);
+
+  po::variables_map vm;
+
+  try {
+    po::store(po::command_line_parser(argc, argv).options(desc)
+              .positional(positionalOptions).run(), vm);
+
+    if (vm.count("help")) {
+        print_usage(app_name, desc);
+        return 0;
+    }
+
+    if (vm.count("stdin")) {
+        stdin = true;
+    }
+
+    po::notify(vm);
+  } catch (boost::program_options::required_option& e) {
+    cerr << "ERROR: " << e.what() << endl << endl;
+    print_usage(app_name, desc);
+    return -1;
+  } catch (boost::program_options::error& e) {
+    cerr << "ERROR: " << e.what() << endl << endl;
+    print_usage(app_name, desc);
+    return -1;
+  }
+
+  if (!stdin && srcpath.empty()) {
+    print_usage(app_name, desc);
+    return -1;
+  }
+
+  if (!stdin) {
+    binary_image(srcpath, dstpath);
   } else {
-    string srcpath, dstpath;
     int processed = 0;
     while (cin >> srcpath >> dstpath) {
-      struct stat st = {0};
-      strncpy(buf, dstpath.c_str(), sizeof(buf));
-      char * dir = dirname(buf);
-      makedirs(dir, 0775);
-      Mat src = imread(srcpath, 1);
-      Mat dst = doit(src);
-      imwrite(dstpath, dst);
+      binary_image(srcpath, dstpath);
       ++processed;
       if (processed % 1000 == 0) {
         cout << "Processed " << processed << " files." << endl;
