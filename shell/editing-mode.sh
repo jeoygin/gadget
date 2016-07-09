75ab98db4e527ac387105c76ea24960ad1e558e2
Jeoygin Wang <jeoygin@gmail.com>
2016-07-09 10:45:13 +0800

    Add shell editing mode

---
+++ shell/editing-mode.sh
@@ -0,0 +1,21 @@
+#
+# Common
+#
+
+$ set -o emacs # Emacs mode
+$ set -o vi # Vi mode
+
+#
+# ZSH
+#
+# http://dougblack.io/words/zsh-vi-mode.html
+
+$ bindkey -e # Emacs mode
+$ bindkey -v # Vi mode
+
+#
+# Bash
+#
+
+$ echo "set editing-mode emacs" >> ~/.inputrc
+$ echo "set editing-mode vi" >> ~/.inputrc
