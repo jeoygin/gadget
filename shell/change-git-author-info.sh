a426dad5acefcfba873a459109f828a2522236f7
Ruijian Wang <ruijian@amazon.com>
2017-08-31 15:48:55 -0700

    Add change-git-author-info.sh

---
+++ shell/change-git-author-info.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# OLD_EMAIL="your-old-email@example.com" NAME="Your Correct Name" EMAIL="your-correct-email@example.com" change-git-author-info
+
+git filter-branch --env-filter '
+OLD_EMAIL="${OLD_EMAIL:?your-old-email@example.com}"
+CORRECT_NAME="${NAME:-Jeoygin Wang}"
+CORRECT_EMAIL="${EMAIL:-jeoygin@gmail.com}"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
